6624a85c7708fbd14e4379b6c984131eafeb5863
Will <will.green98@hotmail.com>
2018-05-02 13:48:06 +0100

    Typo: Maths => Math

--- Tasks 1-50/Task 1/Task-1.py
+++ Tasks 1-50/Task 1/Task-1.py
@@ -1,13 +1,13 @@
 # Task 1 - Python
 # Multiples of 3 and 5
 
-import maths
+import math
 
 count_T = 0
 
-for i in range(maths.pow(10,3)):
+for i in range(math.pow(10,3)):
     if i % 3 == 0 or i % 5 == 0:
-        total += i
+        count_T += i
     print(i)
 print(count_T)
     
