080b8e613559f6c4a513d3e8e294f2eef87e8367
Will <will.green98@hotmail.com>
2018-05-02 13:37:10 +0100

    Task 1 Upload

---
+++ Tasks 1-50/Task 1/Task-1.py
@@ -0,0 +1,13 @@
+# Task 1 - Python
+# Multiples of 3 and 5
+
+import maths
+
+count_T = 0
+
+for i in range(maths.pow(10,3)):
+    if i % 3 == 0 or i % 5 == 0:
+        total += i
+    print(i)
+print(count_T)
+    
