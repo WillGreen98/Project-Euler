bbb53fb5b7ada89bb5dcfd4c04398ec9b1b3fc11
Will <will.green98@hotmail.com>
2018-05-08 23:20:48 +0100

    Attemping to cache the products

--- Tasks 1-50/Task 14/Task-14.py
+++ Tasks 1-50/Task 14/Task-14.py
@@ -3,12 +3,19 @@
 
 import time
 
-n_even = lambda n: n/2
-n_odd = lambda n: (3 * n) + 1
+collatz_rule = lambda n: n / 2 if n % 2 == 0 else ((3 * n) + 1)
 
 def collatz_seq():
+    cache = [0, 0]
 
-    return 0
+    for i in range(1000000):
+        num = collatz_rule(i)
+        
+        if num > cache[0]:
+            cache[0] = num
+            cache[1] = i
+
+    return cache[0], "with Collatz length: ", cache[1]
 
 def main():
     time_start = time.time()
