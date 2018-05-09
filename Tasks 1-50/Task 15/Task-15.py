d8774c4793d95bf073659ff3e2668a41ba39e535
Will <will.green98@hotmail.com>
2018-05-09 19:04:59 +0100

    Task 15

---
+++ Tasks 1-50/Task 15/Task-15.py
@@ -0,0 +1,23 @@
+# Task 14 - Python
+# Longest Collatz sequence
+
+import time
+from functools import reduce
+
+binomial = lambda grid_size: reduce(lambda hoz, vert: hoz * vert, range(1, grid_size + 1), 1)
+
+def path_route_finder(grid_size_to_check):
+    # As size is perfect cube - I have limited calculations instead of n & m
+    size = grid_size_to_check
+    if grid_size_to_check == 0: return 1
+
+    return binomial(2*size) // binomial(size) // binomial(size)
+
+def main():
+    time_start = time.time()
+    route_num = path_route_finder(20)
+
+    print("Answer: {0} => Calculated in: {1}".format(route_num, (time.time() - time_start)))
+
+if __name__ == '__main__':
+    main()
