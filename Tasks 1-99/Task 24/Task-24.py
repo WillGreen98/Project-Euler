78de5a09a917baa0885e20c51db232993b185d63
WillGreen98 <will.green98@hotmail.com>
2018-08-08 22:25:51 +0100

    Pure spicy Maths

--- Tasks 1-99/Task 24/Task-24.py
+++ Tasks 1-99/Task 24/Task-24.py
@@ -8,20 +8,22 @@ from itertools import permutations
 def lexicographical_permutations_cheeky():
     return ''.join(list(permutations(string.digits))[999999])
 
+def permutation(input):
+    lexi_temp = []
 
-def permutation(head, tail=""):
-    if len(head) == 0: print(tail)
-
-    for i in range(len(head)):
-        permutation(head[0:i] + head[i+1:], tail+head[i])
+    for head in range(len(input)):
+        lp = input[:head] + input[head+1:]
+        for tail in permutation(lp):
+            lexi_temp.append(input[head:head+1] + tail)
+    return lexi_temp
 
 def main():
     time_start = time.time()
-    #millionth_lexi_perm = permutation(string.digits)[999999]
+    millionth_lexi_perm = permutation(string.digits)
+    cheeky_lexi_perm = lexicographical_permutations_cheeky()
+    spicy_lexi_perm = divmod(999999, 362880)
 
-    cheeky_spicy_lexi_perm = lexicographical_permutations_cheeky()
-
-    print("Answer: {0} => Calculated in: {1}".format(cheeky_spicy_lexi_perm, (time.time() - time_start)))
+    print("Answer: {0} => Calculated in: {1}".format(millionth_lexi_perm, (time.time() - time_start)))
 
 if __name__ == '__main__':
     main()
