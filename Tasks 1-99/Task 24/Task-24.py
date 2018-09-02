933caf03a1f2c191749445bd3c481117ce510b49
Shwillybum <will.green98@hotmail.com>
2018-09-02 08:09:03 +0100

    Attempt to Fix 24

--- Tasks 1-99/Task 24/Task-24.py
+++ Tasks 1-99/Task 24/Task-24.py
@@ -5,22 +5,22 @@ import time
 import string
 from itertools import permutations
 
-def lexicographical_permutations_cheeky():
-    return ''.join(list(permutations(string.digits))[999999])
+def lexicographical_permutations_cheeky(index):
+    return ''.join(list(permutations(string.digits))[index])
 
-def permutation(input):
+def permutation(data):
     lexi_temp = []
 
-    for head in range(len(input)):
-        lp = input[:head] + input[head+1:]
+    for head in range(len(data)):
+        lp = data[:head] + data[head + 1:]
         for tail in permutation(lp):
-            lexi_temp.append(input[head:head+1] + tail)
+            lexi_temp.append(data[head:head + 1] + tail)
     return lexi_temp
 
 def main():
     time_start = time.time()
     millionth_lexi_perm = permutation(string.digits)
-    cheeky_lexi_perm = lexicographical_permutations_cheeky()
+    cheeky_lexi_perm = lexicographical_permutations_cheeky(999999)
     spicy_lexi_perm = divmod(999999, 362880)
 
     print("Answer: {0} => Calculated in: {1}".format(millionth_lexi_perm, (time.time() - time_start)))
